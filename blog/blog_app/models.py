e702363d499bdc7009f429ba514f02d22ac5eb99
mohamed <mohamedfarhan2113@gmail.com>
2026-08-27 19:54:03 +0100

    use TextField() for body of post insted of CharField()

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='post_owner')
     title = models.CharField(max_length=200)
-    body = models.CharField()
+    body = models.TextField()
 
     def __str__(self):
         return self.title 
